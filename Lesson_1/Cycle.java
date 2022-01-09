3cea36e9cf68c5628f7c91b3bfe41d12795f3acc
Sergey Serebryannikov <s.serebryannikovvv@mail.ru>
2022-01-09 11:51:10 +0300

    errorСorrection

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,23 +1,25 @@
-public class Cycle{
-    public static void main(String[] args){
-        for(int i = 0; i <= 20; i++){
-            System.out.print(i+" ");
+public class Cycle {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            System.out.print(i + " ");
         }
         System.out.println();
+
         int i = 6;
-        while (i>=-6){
-            System.out.print(i+" ");
+        while (i >= -6) {
+            System.out.print(i + " ");
             i--;
         }
         System.out.println();
+
         int j = 10;
-        int sum = 0;
-        do{
-            if(j % 2!=0){
-                sum+=j;
+        int sumOdd = 0;
+        do {
+            if (j % 2 != 0) {
+                sumOdd += j;
             }
             j++;
-        }while (j<=20);
-        System.out.println(sum);
+        } while (j <= 20);
+        System.out.println(sumOdd);
     }
 }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,20 @@
-
-
 public class Variable{
     public static void main(String[] args) {
         byte cores = 8;
         short threads = 16;
         int totalCache = 16384;
-        long systemMemorySpecification = 2_933_000_000l;
+        long systemMemorySpec = 2_933_000_000l;
         float baseClock = 3.2f;
         double maxClock = 4.1;
         char memoryChannels = '2';
         boolean unlockedForOverclocking = true;
+
         System.out.println("Колличество ядер: " + cores);
         System.out.println("Число потоков: " + threads);
         System.out.println("Объем кэш-памяти третьего уровня: " + totalCache);
-        System.out.println("Максимальное быстродействие памяти: " + systemMemorySpecification);
-        System.out.println("Базовая частота: " + baseClock+"GHz");
-        System.out.println("Максимальная частота: " + maxClock+"GHz");
+        System.out.println("Максимальное быстродействие памяти: " + systemMemorySpec);
+        System.out.println("Базовая частота: " + baseClock + "GHz");
+        System.out.println("Максимальная частота: " + maxClock + "GHz");
         System.out.println("Каналы памяти: " + memoryChannels);
         System.out.println("Разблокирован для разгона: " + unlockedForOverclocking);
     }
