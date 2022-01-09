61ba1e919ec12d7d9126c4ab545f13cf43c88ef4
Sergey Serebryannikov <s.serebryannikovvv@mail.ru>
2022-01-09 12:57:29 +0300

    correctionLast

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -8,23 +8,23 @@ public class Calculator {
         System.out.println("Введите второе число: ");
         int secondNumber = scanner.nextInt();
         System.out.println("Введите символ математические операции (+, -, *, /, ^, %): ");
-        char charOperation = scanner.next().charAt(0);
-        int result=0;
+        char mathSign = scanner.next().charAt(0);
+        int result = 0;
         
-        if (charOperation == '+') {
+        if (mathSign == '+') {
             result = firstNumber + secondNumber;
-        } else if (charOperation == '-') {
+        } else if (mathSign == '-') {
             result = firstNumber - secondNumber;
-        } else if (charOperation == '*') {
+        } else if (mathSign == '*') {
             result = firstNumber * secondNumber;
-        } else if (charOperation == '/') {
+        } else if (mathSign == '/') {
             result = firstNumber / secondNumber;
-        } else if (charOperation == '^') {
+        } else if (mathSign == '^') {
             result = 1;
             for (int i = 1; i <= secondNumber; i++) {
-                result = result * firstNumber;
+                result *= firstNumber;
             }
-        }else if(charOperation == '%'){
+        } else if(mathSign == '%') {
             result = firstNumber % secondNumber;
         }
         System.out.println(result);
