1a065c713b81c1b79097c2be7c62839ccce7e02d
Sergey Serebryannikov <s.serebryannikovvv@mail.ru>
2022-01-10 22:03:36 +0300

    firstCommit

---
+++ Lesson_2/Person.java
@@ -0,0 +1,26 @@
+public class Person{
+    String gender = "male";
+    String name = "Ivan";
+    int weight = 85;
+    int age = 25;
+
+    void go() {
+        System.out.println("Идти");
+    }
+
+    void sit() {
+        System.out.println("Сидеть");
+    }
+
+    void run() {
+        System.out.println("Бежать");
+    }
+
+    void speak() {
+        System.out.println("Говорить");
+    }
+
+    void learnJava() {
+        System.out.println("Учим Java!");
+    }
+}
