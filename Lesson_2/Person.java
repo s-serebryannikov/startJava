6051f30e16d5a1eb7c728cba4a06d49838476135
Sergey Serebryannikov <s.serebryannikovvv@mail.ru>
2022-01-11 21:01:06 +0300

    addGetAndSetTask

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,4 +1,4 @@
-public class Person{
+public class Person {
     String gender = "male";
     String name = "Ivan";
     int weight = 85;
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,23 +1,24 @@
-public class WolfTest{
+public class WolfTest {
     public static void main(String[] args) {
-        Wolf wolf1 = new Wolf();
+        Wolf wolf = new Wolf();
 
-        wolf1.gender = "male";
-        wolf1.name = "Tim";
-        wolf1.weight = 25;
-        wolf1.age = 7;
-        wolf1.color = "White";
+        wolf.setGender("male");
+        wolf.setName("Tim");
+        wolf.setWeight(25);
+        wolf.setAge(9);
+        wolf.setColor("White");
 
-        System.out.println("Имя: " + wolf1.name);
-        System.out.println("Пол: " + wolf1.gender);
-        System.out.println("Возраст: " + wolf1.age);
-        System.out.println("Вес: " + wolf1.weight);
-        System.out.println("Цвет: " + wolf1.color);
+        System.out.println("Имя: " + wolf.getName());
+        System.out.println("Пол: " + wolf.getGender());
+        System.out.println("Возраст: " + wolf.getAge());
+        System.out.println("Вес: " + wolf.getWeight());
+        System.out.println("Цвет: " + wolf.getColor());
+        System.out.println();
 
-        wolf1.go();
-        wolf1.run();
-        wolf1.sit();
-        wolf1.howling();
-        wolf1.hunting();
+        wolf.go();
+        wolf.run();
+        wolf.sit();
+        wolf.howl();
+        wolf.hunt();
     }
 }
