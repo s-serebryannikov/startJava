98bb438e618c42e11dc2468d3dd66a3f7eb5edbf
Sergey Serebryannikov <s.serebryannikovvv@mail.ru>
2022-01-26 22:55:01 +0300

    CalcTask*

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -3,42 +3,21 @@ package com.startjava.lesson_2_3_4.calculator;
 import static java.lang.Integer.parseInt;
 
 public class Calculator {
-    private int firstNumber;
-    private char mathSign;
-    private int secondNumber;
-    private String mathExpression;
 
-    public void setMathExpression(String mathExpression) {
-        this.mathExpression = mathExpression;
-    }
+    public static String mathExpression;
 
-    public int calculate() {
-        int result = 0;
-        firstNumber = parseInt(mathExpression.split(" ")[0]);
-        mathSign = mathExpression.split(" ")[1].charAt(0);
-        secondNumber = parseInt(mathExpression.split(" ")[2]);
-        switch (mathSign) {
-            case '+':
-                result = Math.addExact(firstNumber, secondNumber);
-                break;
-            case '-':
-                result = firstNumber - secondNumber;
-                break;
-            case '*':
-                result = firstNumber * secondNumber;
-                break;
-            case '/':
-                result = firstNumber / secondNumber;
-                break;
-            case '^':
-                result = (int) Math.pow(firstNumber, secondNumber);
-                break;
-            case '%':
-                result = firstNumber % secondNumber;
-                break;
-            default:
-                System.out.println("Такой оператор не найден");
-        }
-        return result;
+    public static int calculate() {
+        int firstNumber = parseInt(mathExpression.split(" ")[0]);
+        char mathSign = mathExpression.split(" ")[1].charAt(0);
+        int secondNumber = parseInt(mathExpression.split(" ")[2]);
+        return switch (mathSign) {
+            case '+' -> Math.addExact(firstNumber, secondNumber);
+            case '-' -> firstNumber - secondNumber;
+            case '*' -> firstNumber * secondNumber;
+            case '/' -> firstNumber / secondNumber;
+            case '^' -> (int) Math.pow(firstNumber, secondNumber);
+            case '%' -> firstNumber % secondNumber;
+            default -> 0;
+        };
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,16 +2,23 @@ package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        String answer;
+        String answer = "";
 
+        backToBeginning:
         do {
             System.out.println("Введите математическое выражение: ");
-            calc.setMathExpression(scanner.nextLine());
-            System.out.println("Результат " + calc.calculate());
+            Calculator.mathExpression = scanner.nextLine();
+
+            if (parseInt(Calculator.mathExpression.split(" ")[0]) <= 0 && parseInt(Calculator.mathExpression.split(" ")[0]) % 2 != 1
+                    || parseInt(Calculator.mathExpression.split(" ")[2]) <= 0 && parseInt(Calculator.mathExpression.split(" ")[2]) % 2 != 1) {
+                System.out.println("Не корректные данные\nТребуются целые и положительные числа\n");
+                continue backToBeginning;
+            } else System.out.println("Результат вычисления " + Calculator.calculate());
 
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
