cdfd6a2d4c1f28aa3603786de94dfb7e59c38b4f
Sergey Serebryannikov <s.serebryannikovvv@mail.ru>
2022-01-29 13:16:43 +0300

    correctionCalc

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,23 +1,34 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import static java.lang.Integer.parseInt;
+import java.util.Scanner;
+
+import static java.lang.Double.parseDouble;
 
 public class Calculator {
 
-    public static String mathExpression;
-
-    public static int calculate() {
-        int firstNumber = parseInt(mathExpression.split(" ")[0]);
-        char mathSign = mathExpression.split(" ")[1].charAt(0);
-        int secondNumber = parseInt(mathExpression.split(" ")[2]);
-        return switch (mathSign) {
-            case '+' -> Math.addExact(firstNumber, secondNumber);
-            case '-' -> firstNumber - secondNumber;
-            case '*' -> firstNumber * secondNumber;
-            case '/' -> firstNumber / secondNumber;
-            case '^' -> (int) Math.pow(firstNumber, secondNumber);
-            case '%' -> firstNumber % secondNumber;
-            default -> 0;
-        };
+    public static void calculate(String mathExpression) {
+        if (parseDouble(mathExpression.split(" ")[0]) % 1 == 0 & parseDouble(mathExpression.split(" ")[0]) > 0
+                & parseDouble(mathExpression.split(" ")[2]) % 1 == 0 & parseDouble(mathExpression.split(" ")[2]) > 0) {
+            int firstNumber = Integer.parseInt(mathExpression.split(" ")[0]);
+            char mathSign = mathExpression.split(" ")[1].charAt(0);
+            int secondNumber = Integer.parseInt(mathExpression.split(" ")[2]);
+            int math = switch (mathSign) {
+                case '+' -> Math.addExact(firstNumber, secondNumber);
+                case '-' -> firstNumber - secondNumber;
+                case '*' -> firstNumber * secondNumber;
+                case '/' -> firstNumber / secondNumber;
+                case '^' -> (int) Math.pow(firstNumber, secondNumber);
+                case '%' -> firstNumber % secondNumber;
+                default -> 0;
+            };
+            System.out.println("Результат вычисления: " + math);
+        } else {
+            System.out.println("Не корректные значения\nВведите значения ещё раз");
+            System.out.println("Введите математическое выражение: ");
+            Scanner scanner = new Scanner(System.in);
+            Calculator.calculate(scanner.nextLine());
+        }
     }
 }
+
+
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,28 +2,20 @@ package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
-
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String answer = "";
 
-        backToBeginning:
+        System.out.println("Введите математическое выражение: ");
+        Calculator.calculate(scanner.nextLine());
         do {
-            System.out.println("Введите математическое выражение: ");
-            Calculator.mathExpression = scanner.nextLine();
-
-            if (parseInt(Calculator.mathExpression.split(" ")[0]) <= 0 && parseInt(Calculator.mathExpression.split(" ")[0]) % 2 != 1
-                    || parseInt(Calculator.mathExpression.split(" ")[2]) <= 0 && parseInt(Calculator.mathExpression.split(" ")[2]) % 2 != 1) {
-                System.out.println("Не корректные данные\nТребуются целые и положительные числа\n");
-                continue backToBeginning;
-            } else System.out.println("Результат вычисления " + Calculator.calculate());
-
-            do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                answer = scanner.nextLine();
-            } while (!answer.equals("yes") & !answer.equals("no"));
+            System.out.println("Хотите продолжить вычисления? [yes/no]:");
+            answer = scanner.nextLine();
+            if (answer.equals("yes")) {
+                System.out.println("Введите математическое выражение: ");
+                Calculator.calculate(scanner.nextLine());
+            }
         } while (!answer.equals("no"));
     }
 }
